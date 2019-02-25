70d7a2ef6cb16fb31f99a0c00562c866c8809642
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2019-02-25 10:42:45 +0100

    Programa de prueba

--- Implementacion/main.py
+++ Implementacion/main.py
@@ -1,2 +1,12 @@
 #!# -*- coding: utf-8 -*-
-import mahalanobis_kernel
+import numpy as np
+from KernelMahalanobis import KernelMahalanobis
+
+data_file = open("./datasets/yeast/yeast.data")
+dataset = []
+for line in data_file:
+    # Removing que first and last column
+    dataset.append(list(map(float,list(filter(None,line.split(" ")))[1:-1])))
+
+kernel_mahalanobis = KernelMahalanobis(10,np.array(dataset),len(dataset))
+kernel_mahalanobis.runMethod()
