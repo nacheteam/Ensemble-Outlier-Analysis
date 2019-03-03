d8337dbddb721f56703f304484d6591abc9ee30e
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2019-03-03 23:33:03 +0100

    Main mejorado con gráfica

--- Implementacion/main.py
+++ Implementacion/main.py
@@ -1,6 +1,7 @@
 #!# -*- coding: utf-8 -*-
 import numpy as np
 from KernelMahalanobis import KernelMahalanobis
+import matplotlib.pyplot as plt
 
 data_file = open("./datasets/yeast/yeast.data")
 dataset = []
@@ -10,4 +11,15 @@ for line in data_file:
 
 # I place already the dataset as a matrix
 kernel_mahalanobis = KernelMahalanobis(10,np.matrix(dataset),len(dataset))
-kernel_mahalanobis.runMethod()
+scores = np.array(kernel_mahalanobis.runMethod())
+print("Media de los scores: " + str(np.mean(scores)))
+
+border = 0.9*(max(list(scores))-min(list(scores))) + min(list(scores))
+outliers = np.where(scores>=border)
+plt.scatter(list(range(len(scores))),scores,c="b")
+plt.scatter(np.array(list(range(len(scores))))[outliers],scores[outliers],c="r")
+plt.xlabel("Dato")
+plt.ylabel("Score")
+plt.title("Scatter Plot de los scores, en rojo las anomalías")
+plt.legend()
+plt.show()
