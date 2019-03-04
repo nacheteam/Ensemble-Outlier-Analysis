e6d38ea737df584359d42c5c147a14e2c53424ec
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2019-03-04 10:53:13 +0100

    TODO y mejor formato de las gráficas

--- Implementacion/main.py
+++ Implementacion/main.py
@@ -3,7 +3,7 @@ import numpy as np
 from KernelMahalanobis import KernelMahalanobis
 import matplotlib.pyplot as plt
 
-NUM_ITERACIONES = 25
+NUM_ITERACIONES = 10
 np.random.seed(12345)
 
 ################################################################################
@@ -19,7 +19,7 @@ def readData():
         numerical = list(map(float,list(filter(None,line.split(" ")))[1:-1]))
         numerical.append(classes[line.split(" ")[-1].strip()])
         dataset.append(numerical)
-    return dataset
+    return np.matrix(dataset)
 
 ################################################################################
 ##                                  Main                                      ##
@@ -29,18 +29,19 @@ def main():
     dataset = readData()
 
     # I place already the dataset as a matrix
-    kernel_mahalanobis = KernelMahalanobis(NUM_ITERACIONES,np.matrix(dataset),len(dataset))
+    kernel_mahalanobis = KernelMahalanobis(NUM_ITERACIONES,dataset,len(dataset))
     scores = np.array(kernel_mahalanobis.runMethod())
     print("Media de los scores: " + str(np.mean(scores)))
 
     border = 0.9*(max(list(scores))-min(list(scores))) + min(list(scores))
     outliers = np.where(scores>=border)
-    plt.scatter(list(range(len(scores))),scores,c="b")
-    plt.scatter(np.array(list(range(len(scores))))[outliers],scores[outliers],c="r")
+    print("Los datos anómalos son los que tienen índices: " + str(list(outliers)))
+    plt.scatter(list(range(len(scores))),scores,c="b",label="Datos no anómalos")
+    plt.scatter(np.array(list(range(len(scores))))[outliers],scores[outliers],c="r",label="Anomalías")
     plt.xlabel("Dato")
     plt.ylabel("Score")
     plt.title("Scatter Plot de los scores, en rojo las anomalías")
     plt.legend()
     plt.show()
-
+    # TODO: Plot the data seaborn-like for every pair of features
 main()
