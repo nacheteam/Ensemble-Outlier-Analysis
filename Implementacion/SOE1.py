280b35ab670442e8fd3611cdf0ec46e955f4de03
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2019-03-27 09:28:48 +0100

    Método del algoritmo SOE1

--- Implementacion/SOE1.py
+++ Implementacion/SOE1.py
@@ -15,20 +15,22 @@ class SOE1:
     comb: combination/ensemble function
     '''
 
-    def __init__(self,k,SS,comb,dataset):
+    def __init__(self,k,SS,comb,dataset,score_comb):
         '''
         @brief Function that initialises the SOE1 class
         @param self
         @param k Number of outliers wanted to be extracted
         @param SS list of subspaces of the characteristics given as lists of indexes
         in the dataset
-        @param comb Combination function should take a bunch of histograms and combine
-        them into one single histogram.
+        @param comb Combination function should take a bunch of histograms in a numpy
+        array objetct and combine them into one single histogram.
+        @param score_comb Combination function for the outlierness scores
         '''
         self.k = k
         self.SS = SS
         self.comb = comb
         self.dataset = dataset
+        self.score_comb = score_comb
 
     def runMethod(self):
         '''
@@ -37,11 +39,17 @@ class SOE1:
         @param self
         '''
         dimension = len(self.dataset[0])
-        histograms = []
+        histograms1D = []
         for i in range(dimension):
             subset = self.dataset[:,i]
             unique, counts = numpy.unique(subset, return_counts=True)
-            histograms.append(zip(unique,counts))
+            histograms1D.append(zip(unique,counts))
+        histograms1D = np.array(histograms1D)
+        histogramsSS = []
+        for subs in self.SS:
+            histogramsSS.append(self.comb(histograms1D[subs]))
+        histogramsSS = np.array(histogramsSS)
+        self.scores = self.score_comb(np.append(histograms1D,histogramsSS))[:k]
 
     def obtainResults(self):
         '''
