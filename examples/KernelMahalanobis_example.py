04cda4ec79f60c3b8092af8320d438e87b2f2600
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2019-06-26 18:14:05 +0200

    Uses abalone to test

--- examples/KernelMahalanobis_example.py
+++ examples/KernelMahalanobis_example.py
@@ -1,71 +1,56 @@
 #!# -*- coding: utf-8 -*-
+import sys
+sys.path.append('../')
+
 import numpy as np
 from KernelMahalanobis import KernelMahalanobis
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-NUM_ITERACIONES = 1000
-np.random.seed(12345)
+NUM_ITERACIONES = 100
+np.random.seed(123456789)
 
 ################################################################################
 ##                      Función de lectura del dataset                        ##
 ################################################################################
 
 def readData():
-    data_file = open("./datasets/yeast/yeast.data")
+    data_file = open("../datasets/abalone19.dat")
     dataset = []
-    classes = {"CYT":0,"NUC":1,"MIT":2,"ME3":3,"ME2":4,"ME1":5,"EXC":6,"VAC":7,"POX":8,"ERL":9}
+    labels = []
+    # To parse classes and sex column to numerical
+    classes = {"negative":0,"positive":1}
+    sex = {"M":0, "F":1, "I":2}
+    reading_data=False
     for line in data_file:
-        # Removing que first column
-        numerical = list(map(float,list(filter(None,line.split(" ")))[1:-1]))
-        numerical.append(classes[line.split(" ")[-1].strip()])
-        dataset.append(numerical)
-    return np.matrix(dataset)
-
-def createDataFrame(outliers):
-    feature_names = ["mcg","gvh","alm","mit","erl","pox","vac","nuc"]
-    data_file = open("./datasets/yeast/yeast.data")
-    dataset = []
-    i=0
-    for line in data_file:
-        # Removing the first column
-        numerical = list(map(float,list(filter(None,line.split(" ")))[1:-1]))
-        cl = line.split(" ")[-1].strip() if i not in outliers else "outlier"
-        numerical.append(cl)
-        dataset.append(numerical)
-        i+=1
-    return pd.DataFrame(data = dataset,columns = feature_names+["classes"])
-
-def allPossiblePairs(list):
-    pairs = []
-    for i in range(len(list)):
-        for j in range(i+1,len(list)):
-            pairs.append([list[i],list[j]])
-    return pairs
+        if reading_data:
+            vars = line.rstrip().split(",")
+            row=[]
+            for i in range(len(vars)):
+                if i==0:
+                    row.append(sex[vars[i].strip()])
+                elif i==len(vars)-1:
+                    labels.append(classes[vars[i].strip()])
+                else:
+                    row.append(float(vars[i]))
+            dataset.append(row)
+        if "@data" in line and not reading_data:
+            reading_data = True
+    return np.matrix(dataset), np.array(labels)
 
 ################################################################################
 ##                                  Main                                      ##
 ################################################################################
 
 def main():
-    dataset = readData()
+    dataset,labels = readData()
 
     # I place already the dataset as a matrix
     kernel_mahalanobis = KernelMahalanobis(NUM_ITERACIONES,dataset,len(dataset))
-    kernel_mahalanobis.runMethod()
+    kernel_mahalanobis.runMethodClassic()
     kernel_mahalanobis.obtainResults()
 
-    # TODO: Plot the data seaborn-like for every pair of features
-    data_frame = createDataFrame(kernel_mahalanobis.outliers)
-
-    classes_palette = {"CYT":"C0","NUC":"C1","MIT":"C2","ME3":"C3","ME2":"C4","ME1":"C5","EXC":"C6","VAC":"C7","POX":"C8","ERL":"C9","outlier":"k"}
-    pairs = allPossiblePairs(["mcg","gvh","alm","mit","erl","pox","vac","nuc"])
-    i=0
-    for p in pairs:
-        print("Pair " + str(i+1) + "/" + str(len(pairs)))
-        i+=1
-        sns.pairplot(data_frame,hue="classes",diag_kind="hist",vars=p,palette=classes_palette)
-        plt.show()
+    print(labels[kernel_mahalanobis.outliers])
 
 main()
