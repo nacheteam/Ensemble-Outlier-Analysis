874caa3ced3af9b8a8e63f13f32c1f3d050f152c
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2019-06-27 18:31:40 +0200

    Modified example fitting the new template

--- examples/KernelMahalanobis_example.py
+++ examples/KernelMahalanobis_example.py
@@ -1,6 +1,6 @@
 #!# -*- coding: utf-8 -*-
 import sys
-sys.path.append('../')
+sys.path.append('../models/')
 
 import numpy as np
 from KernelMahalanobis import KernelMahalanobis
@@ -9,13 +9,14 @@ import pandas as pd
 import seaborn as sns
 
 NUM_ITERACIONES = 100
+NOUTLIERS=10
 np.random.seed(123456789)
 
 ################################################################################
 ##                      Función de lectura del dataset                        ##
 ################################################################################
 
-def readData():
+def readDataAbalone():
     data_file = open("../datasets/abalone19.dat")
     dataset = []
     labels = []
@@ -39,18 +40,66 @@ def readData():
             reading_data = True
     return np.matrix(dataset), np.array(labels)
 
+def readDataYeast():
+    data_file = open("../datasets/yeast6.dat")
+    dataset = []
+    labels = []
+    # To parse classes to numerical
+    classes = {"negative":0,"positive":1}
+    reading_data=False
+    for line in data_file:
+        if reading_data:
+            vars = line.rstrip().split(",")
+            row=[]
+            for i in range(len(vars)):
+                if i==len(vars)-1:
+                    labels.append(classes[vars[i].strip()])
+                else:
+                    row.append(float(vars[i]))
+            dataset.append(row)
+        if "@data" in line and not reading_data:
+            reading_data = True
+    return np.matrix(dataset), np.array(labels)
+
+def obtainResults(mk):
+    print("\n\n\n##########################################################")
+    print("Statistical information about the outliers")
+    print("##########################################################\n\n")
+    scores = mk.getRawScores()
+    # First the mean, stdv and variance
+    mean = np.mean(scores)
+    stdv = np.std(scores)
+    variance = np.var(scores)
+    print("The mean of the scores is: " + str(mean))
+    print("The standard deviation of the scores is: " + str(stdv))
+    print("The variance of the scores is: " + str(variance))
+
+    outliers = mk.getOutliersBN(NOUTLIERS)
+    print("The outliers are the elements with indexes: " + str(outliers))
+
+    # Then we will plot the scores colouring the outliers in red.
+    plt.scatter(list(range(len(scores))),scores,c="b",label="Normal data")
+    plt.scatter(np.array(list(range(len(scores))))[outliers],scores[outliers],c="r",label="Outliers")
+    plt.xlabel("Data")
+    plt.ylabel("Score")
+    plt.title("Scatter Plot of the scores, outliers in red.")
+    plt.legend()
+    plt.show()
+
 ################################################################################
 ##                                  Main                                      ##
 ################################################################################
 
 def main():
-    dataset,labels = readData()
+    #dataset,labels = readDataAbalone()
+    dataset,labels = readDataYeast()
 
     # I place already the dataset as a matrix
-    kernel_mahalanobis = KernelMahalanobis(NUM_ITERACIONES,dataset,len(dataset))
-    kernel_mahalanobis.runMethodClassic()
-    kernel_mahalanobis.obtainResults()
+    kernel_mahalanobis = KernelMahalanobis(NUM_ITERACIONES)
+    kernel_mahalanobis.fit(dataset)
+    kernel_mahalanobis.runMethod()
+    obtainResults(kernel_mahalanobis)
 
-    print(labels[kernel_mahalanobis.outliers])
+    print(labels[kernel_mahalanobis.getOutliersBN(NOUTLIERS)])
 
 main()
