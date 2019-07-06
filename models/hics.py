d60b77c7169a88a367580b30ba5d004c907d9fe0
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2019-07-06 19:10:53 +0200

    Function to generate the subspaces

--- models/hics.py
+++ models/hics.py
@@ -1,5 +1,6 @@
 from base import EnsembleTemplate
 import numpy as np
+from itertools import combinations
 
 OUTLIER_RANKING_POS = ["lof", "cof", "cblof", "loci", "hbos", "sod"]
 
@@ -66,5 +67,44 @@ class HICS(EnsembleTemplate):
         return max
 
     def generateSubspaces(self):
-        for dim in range(2,len(self.dataset[0])):
-            
+        # Ordered subspaces by dimension type: list of numpy arrays of numpy arrays
+        # this means that in each positions there would be the subspaces of the corresponding
+        # dimension in the form of a list of subspaces which are numpy arrays
+        all_subspaces = []
+        all_contrasts = []
+        for dimension in range(2,len(self.dataset[0])):
+            candidates = np.array([])
+            contrasts = np.array([])
+            redundant = []
+            # For dimension 2 we just obtain all possible indexes and make all combinations
+            if dimension==2:
+                # Calculate the candidates
+                indexes = list(range(len(self.dataset[0])))
+                candidates = np.array([np.array(list(comb)) for comb in list(combinations(indexes,dimension))])
+                # Compute the contrasts
+                contrasts = np.array([self.computeContrast(can) for can in candidates])
+
+            else:
+                # We need to calculate now the indexes starting from a previous subspace
+                for i in range(len(all_subspaces[-1])):
+                    indexes = list(set(list(range(len(self.dataset[0])))).difference(set(all_subspaces[-1][i])))
+                    parent_should_be_removed = False
+                    for ind in indexes:
+                        candidates = np.append(candidates,np.append(all_subspaces[-1][i],ind))
+                        contrasts = np.append(contrasts, self.computeContrast(candidates[-1]))
+                        if contrasts[-1]>all_contrasts[-1][i]:
+                            parent_should_be_removed=True
+                    if parent_should_be_removed:
+                        redundant.append(i)
+            non_redundant_sub = np.delete(all_subspaces[-1], redundant)
+            all_subspaces[-1]=non_redundant_sub
+            # Sort from higher contrast to lower and only get numCandidates number of subspaces if available
+            if len(candidates)>self.numCandidates:
+                all_subspaces.append(candidates[contrasts.argsort()[-self.numCandidates:][::-1]])
+                all_contrasts.append(contrasts[contrasts.argsort()[-self.numCandidates:][::-1]])
+            else
+                all_subspaces.append(candidates)
+                all_contrasts.append(contrasts)
+        subspaces = np.array(all_subspaces).flatten()
+        contrasts = np.array(all_contrasts).flatten()
+        return subspaces[contrasts.argsort()[-self.maxOutputSpaces:][::-1]]
