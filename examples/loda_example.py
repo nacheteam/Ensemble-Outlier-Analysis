de33f58eedd794d0f8992d45436dfbfbe6080153
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2019-08-17 19:06:53 +0200

    Loda example

---
+++ examples/loda_example.py
@@ -0,0 +1,53 @@
+#!# -*- coding: utf-8 -*-
+import sys
+sys.path.append('../models/')
+sys.path.append("../test/")
+
+import numpy as np
+from loda import LODA
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+import utils
+
+from pyod_test import voteForOutliers
+
+from cProfile import Profile
+import pstats, io
+
+np.random.seed(123456789)
+
+################################################################################
+##                                  Main                                      ##
+################################################################################
+
+def main():
+    #dataset,labels = utils.readDataAbalone()
+    dataset,labels = utils.readDataYeast()
+    #dataset, labels = utils.readDataCancer()
+
+    loda = LODA()
+    '''
+    p = Profile()
+    p.enable()
+    '''
+    loda.fit(dataset)
+    '''
+    p.disable()
+    s = io.StringIO()
+    ps = pstats.Stats(p, stream=s).sort_stats('cumulative')
+    ps.print_stats()
+    print(s.getvalue())
+    utils.obtainResults(loda)
+    '''
+    outliers = loda.getOutliers()
+
+    print(labels[outliers])
+
+    print("Getting anomalies based on the voting system to check")
+    cm, df = utils.checkAnomalies(dataset, outliers)
+    print("Common ones: " + str(cm))
+    print("Different ones: " + str(df))
+
+main()
