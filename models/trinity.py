bd61345d10c3832d67dfd2c7a331764d23c912cd
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2019-08-18 11:10:51 +0200

    Comments on trinity

--- models/trinity.py
+++ models/trinity.py
@@ -12,6 +12,7 @@ class TRINITY(EnsembleTemplate):
 
     Parameters
     ------------
+    * num_iter: number of iterations to repeat the sampling process
 
     """
 
@@ -26,51 +27,84 @@ class TRINITY(EnsembleTemplate):
         self.verbose = verbose
 
     def distanceBased(self):
+        '''
+        @brief Function that implements the distance based component
+        @param self
+        @return It returns the vector with the scores of the instances
+        '''
+        # Initialize the scores
         scores = np.array([0]*len(self.dataset)).astype(float)
         for i in range(self.num_iter):
             knn = KNN(n_neighbors=5, contamination=self.contamination)
             # Number in the interval [50, 1000]
             subsample_size = np.random.randint(50, 1001)
+            # Take the sample and train the model
             sample = np.random.choice(len(self.dataset), size=subsample_size, replace=False)
             knn.fit(self.dataset[sample])
+            # Update the score to compute the mean
             scores[sample]+=knn.decision_scores_
+        # Return the mean
         return scores/self.num_iter
 
     def dependencyBased(self):
+        '''
+        @brief Function that implements the dependency based component
+        @param self
+        @return It returns the vector with the scores of the instances
+        '''
+        # Initialize the scores
         scores = np.array([0]*len(self.dataset)).astype(float)
         for i in range(self.num_iter):
             kernel_mahalanobis = KernelMahalanobis(contamination=self.contamination)
             # Number in the interval [50, 1000]
             subsample_size = np.random.randint(50, 1001)
+            # Take the sample and train the model
             sample = np.random.choice(len(self.dataset), size=subsample_size, replace=False)
             kernel_mahalanobis.fit(self.dataset[sample])
+            # Update the score to compute the mean
             scores[sample]+=kernel_mahalanobis.outlier_score
+        # Return the mean
         return scores/self.num_iter
 
     def densityBased(self):
+        '''
+        @brief Function that implements the dependency based component
+        @param self
+        @return It returns the vector with the scores of the instances
+        '''
+        # Initialize the scores
         scores = np.array([0]*len(self.dataset)).astype(float)
         for i in range(self.num_iter):
             iforest = IForest(contamination=self.contamination)
             # Number in the interval [50, 1000]
             subsample_size = np.random.randint(50, 1001)
+            # Take the sample and train the model
             sample = np.random.choice(len(self.dataset), size=subsample_size, replace=False)
             iforest.fit(self.dataset[sample])
+            # Update the score to compute the mean
             scores[sample]+=iforest.decision_scores_
+        # Return the mean
         return scores/self.num_iter
 
     def runMethod(self):
         '''
         @brief This function is the actual implementation of TRINITY
+        @param self
         '''
+        # Distance module
         if self.verbose:
             print("Obtaining scores with the distance module")
         distance_based = self.distanceBased()
+        # dependency module
         if self.verbose:
             print("Obtaining scores with the dependency module")
         dependency_based = self.dependencyBased()
+        # Density module
         if self.verbose:
             print("Obtaining scores with the density module")
         density_based = self.densityBased()
+
+        # Compute the mean of the three modules
         self.outlier_score=(distance_based + dependency_based + density_based)/3
         self.calculations_done=True
 
